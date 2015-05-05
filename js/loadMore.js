d4072e209ef5d21d3bf0213f8e14f7de852a9b60
rainyjune <rainyjune@live.cn>
2015-05-05 08:37:37 +0800

    First commit

---
+++ js/loadMore.js
@@ -0,0 +1,64 @@
+/**
+ * @fileOverview 微信一分钱回家-加载更多模块
+ * @author rainyjune <rainyjune@live.cn>
+ * @version $Id: loadMore.js 724557 2015-01-26 07:42:15Z chenkang $
+ */
+  
+  $(function(){
+    
+    $("a.d-more").on("click", function(){
+      var url = $("#load_more_url").val();
+      showLoadingIcon();
+      $.getJSON(url, function(data){
+        //alert("Result Data:["+data+"]");
+        //alert("typeof data:" + (typeof data));
+        //console.log(data);
+        if ($.isArray(data) && data.length > 0) {
+          //alert("is array");
+          $("a.d-more").hide();
+          handleMoreArray(data);
+        } else {
+          //alert("not a array");
+        }
+      });
+      return false;
+    });
+    
+    //ganxie
+    $(".d-friends-word").on("click", "a.d-right", function(){
+      var text = $(this).parent().find("span.win_text").html();
+      $(".sCityFixBox").hide();
+      $("#ganxie").find(".thankBox").html("<p>" + text + "</p>");
+      $("#ganxie").show();
+      return false;
+    });
+    
+    //baoguang
+    $(".d-friends-word").on("click", "a.d-a1", function(){
+      var text = $(this).parent().find("span.win_text").html();
+      $(".sCityFixBox").hide();
+      $("#baoguang").find(".thankBox").html("<p>" + text + "</p>");
+      $("#baoguang").show();
+      return false;
+    });
+    
+    $("#ganxie, #baoguang").on("click", function(){
+      $(this).hide();
+      return false;
+    });
+  });
+  
+  function showLoadingIcon() {
+    $("a.d-more").html("<img src='http://src.house.sina.com.cn/imp/imp/deal/e7/0b/f/b4cf7680895aa192750cc57f74b_p10_mk10.gif' width='19' height='19' />");
+  }
+  
+  function handleMoreArray(arr) {
+    var theArr = [];
+    $.each(arr, function(index, item) {
+      var str = friendTemplate(item).outerHTML;
+      theArr.push(str);
+    });
+    var theLongString = theArr.join("");
+    $(".d-friends-word").append(theLongString);
+  }
+  
---
+++ js/main.js
@@ -0,0 +1,115 @@
+/**
+ * @fileOverview 微信一分钱回家-转盘页
+ * @author rainyjune <rainyjune@live.cn>
+ */
+  
+  $(function(){
+    var is_turn = $("#is_turn").val();
+    var isSelf = $("#is_self").val();
+    var lukyUsers = $(".d-lottery-list-container").children();
+    
+    
+    $("#panResult").on("click", "a.close", function(){
+      $("#panResult").hide();
+      return false;
+    });
+    
+    if (is_turn > 0) {
+      rouletteGame();
+    }
+    
+    if (isSelf != 1 && is_turn ==0) {
+      $("a.evHand").on("click", function(){
+        showTip("亲，不能帮同一好友重复筹款哦~");
+        return false;
+      });
+    }
+    
+    
+    //$(".d-lottery-list").autoScroll();
+    if (lukyUsers.length > 2) {
+      //code
+      $(".d-lottery-list").autoScroll();
+    }
+    
+    $("a.popup").on("click", function(){
+      $("#shareBoxContainer").show();
+      return false;
+    });
+    
+    $("#shareBoxContainer").on("click",  function(){
+      $(this).hide();
+    });
+  });
+  
+  function showTip(msg) {
+    $("#wrongTipBox").find(".wrong").html(msg);
+    $("#wrongTipBox").show();
+    setTimeout(function(){
+      $("#wrongTipBox").hide();
+    }, 2000);
+  }
+  
+  // 轮盘赌
+  function rouletteGame() {
+    var shownum = document.getElementById('shownum');
+    var eles = {};
+    eles.radiusbox = document.getElementById('radiusbox'); // 轮盘外框
+    eles.box = eles.radiusbox.getElementsByClassName('box')[0]; // 轮盘
+    eles.rot = eles.box.getElementsByClassName('rot')[0]; // 要转的元素
+    eles.hand = eles.box.getElementsByClassName('hand')[0]; // 指针
+    eles.evHand = eles.box.getElementsByClassName('evHand')[0]; // 指针点击按钮
+    
+    // exe
+    var game = roulette({
+      requestUrl: document.getElementById("wheel_draw_url").value,
+      radiusbox : eles.radiusbox,
+      box : eles.box,
+      rot : eles.rot,
+      hand: eles.hand,
+      evHand: eles.evHand,
+      clickTime : 6000000,
+      radius : $("#items span").length,
+      fnClickEnd: function(n){
+        //alert('恭喜您获得'+n.money+'等奖');
+        var resultMoneyTotal = $("#resultMoneyTotal");
+        resultMoneyTotal.text(parseInt(resultMoneyTotal.text()) + parseInt(n.money) + "元");
+        
+        var helpCount = $("#helpCount");
+        helpCount.text(parseInt(helpCount.text()) + 1);
+        
+        $("#resultMoney").text(n.money);
+        $(".expenses").find("i").html(n.money >= 0 ? "¥" + n.money : "¥<em>-" + Math.abs(n.money) + "</em>");
+        $("#panResult").show();
+        
+        if (n.remain_num == 0) {
+          $("#panResult").one("click", "a.close", function(){
+            document.location.reload(true);
+            return false;
+          });
+        }
+
+        // insert this record to the activity list
+        var str = friendTemplate(n).outerHTML;
+        //console.log("str", str);
+        $("div.d-friends-word").prepend(str);
+        $("div.d-friends-box").removeClass("none");
+        // Update the load_more_url value
+        var loadMoreUrl = $("#load_more_url").val();
+        var oldOffsetResult = loadMoreUrl.match(/&offset=(\d+)&/);
+        if (oldOffsetResult && oldOffsetResult[1] && !isNaN(oldOffsetResult[1])) {
+          var oldOffset = parseInt(oldOffsetResult[1]);
+          var theNewUrl = loadMoreUrl.replace(/&offset=(\d+)&/, "&offset="+ (oldOffset + 1) +"&");
+          
+          //console.log("theNewUrl:", theNewUrl);
+          $("#load_more_url").val(theNewUrl);
+        }
+      },
+      gameOver: function(){
+        showTip("亲，不能帮同一好友重复筹款哦~");
+        setTimeout(function(){
+          document.location.reload(true);
+        }, 2000);
+      }
+    });
+  }
